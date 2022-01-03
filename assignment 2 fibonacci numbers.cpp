b5dea86250cd9da2f91e4f7100a9d97b215490b0
Shah-jahan Mukhtar <Shahjahanmukhtar62@gmail.com>
2022-01-03 13:44:59 +0500

    programming fundaments and OOP

--- assignment 2 fibonacci numbers.cpp
+++ assignment 2 fibonacci numbers.cpp
@@ -1,24 +1,23 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
+
 void printfibonaccinumbers(int n)
 {
-	int f1=0,f2=1,i;
-	if (n<1)
-	return ;
-	for (i=1;i<=n;i++)
+	int f1 = 0, f2 = 1, i;
+	if (n < 1)
+		return;
+	for (i = 1; i <= n; i++)
 	{
-		cout<<f2<<""<<endl;
-		int next=f1+f2;
-		f1=f2;
-		f2=next;
-		
+		cout << f2 << "" << endl;
+		int next = f1 + f2;
+		f1 = f2;
+		f2 = next;
 	}
 }
 
-	int main ()
-	{
-		printfibonaccinumbers(5);
-		return 0;
-	}
-
+int main()
+{
+	printfibonaccinumbers(5);
+	return 0;
+}
